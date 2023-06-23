41fc55e5905023cbd9ca89d0a072168e4a8cf45d
Francois GONZALEZ <fgonzale@bess-f3r4s9.clusters.42paris.fr>
2023-06-23 18:14:59 +0200

    more norm fix

--- src/map_creation.c
+++ src/map_creation.c
@@ -6,7 +6,7 @@
 /*   By: fgonzale <fgonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:32:45 by fgonzale          #+#    #+#             */
-/*   Updated: 2023/06/23 15:51:34 by fgonzale         ###   ########.fr       */
+/*   Updated: 2023/06/23 18:13:04 by fgonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,25 @@ void	load_xpm_files(t_data *data)
 	check_xpm_load(data);
 }
 
+static void	put_images(t_data *data, char c, int i, int j)
+{	
+	if (c == '1')
+		mlx_put_image_to_window(data->mlx_ptr, data->mlx_window,
+			data->textures.wall, j * SPRITE_SIZE, i * SPRITE_SIZE);
+	else if (c == 'C')
+		mlx_put_image_to_window(data->mlx_ptr, data->mlx_window,
+			data->textures.collectibles, j * SPRITE_SIZE, i * SPRITE_SIZE);
+	else if (c == '0')
+		mlx_put_image_to_window(data->mlx_ptr, data->mlx_window,
+			data->textures.floor, j * SPRITE_SIZE, i * SPRITE_SIZE);
+	else if (c == 'E')
+		mlx_put_image_to_window(data->mlx_ptr, data->mlx_window,
+			data->textures.exit, j * SPRITE_SIZE, i * SPRITE_SIZE);
+	else if (c == 'P')
+		mlx_put_image_to_window(data->mlx_ptr, data->mlx_window,
+			data->textures.player, j * SPRITE_SIZE, i * SPRITE_SIZE);
+}
+
 void	create_map(t_data *data)
 {
 	int	i;
@@ -54,17 +73,6 @@ void	create_map(t_data *data)
 	{
 		j = -1;
 		while (data->map[i][++j])
-		{
-			if (data->map[i][j] == '1')
-				mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, data->textures.wall, j * SPRITE_SIZE, i * SPRITE_SIZE);
-			else if (data->map[i][j] == 'C')
-				mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, data->textures.collectibles, j * SPRITE_SIZE, i * SPRITE_SIZE);
-			else if (data->map[i][j] == '0')
-				mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, data->textures.floor, j * SPRITE_SIZE, i * SPRITE_SIZE);
-			else if (data->map[i][j] == 'E')
-				mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, data->textures.exit, j * SPRITE_SIZE, i * SPRITE_SIZE);
-			else if (data->map[i][j] == 'P')
-				mlx_put_image_to_window(data->mlx_ptr, data->mlx_window, data->textures.player, j * SPRITE_SIZE, i * SPRITE_SIZE);
-		}
+			put_images(data, data->map[i][j], i, j);
 	}
 }
